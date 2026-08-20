7a07b9488d9ff8882d7b98a0742de30c2cc4fbb5
Marcio Matos <cromo.jml@gmail.com>
2026-08-20 15:28:55 +0200

    update environment variable handling

--- src/task_assistant/utils/constants.py
+++ src/task_assistant/utils/constants.py
@@ -1,3 +1,5 @@
-MLFLOW_TRACKING_URI="http://localhost:5000"
-MLFLOW_EXPERIMENT_NAME="task-assistant"
-DATABASE_URL="sqlite:///./task_assistant.db"
+import os
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
+MLFLOW_EXPERIMENT_NAME = os.getenv("MLFLOW_EXPERIMENT_NAME", "task-assistant")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./task_assistant.db")
